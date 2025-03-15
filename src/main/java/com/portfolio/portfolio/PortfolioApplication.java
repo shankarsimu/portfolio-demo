57386b992cd4b66df3ec905a9a04857c5d687376
Shanakrsimu <Shankarnarayan791@gmail.com>
2025-03-15 16:33:02 +0530

    new update

---
+++ src/main/java/com/portfolio/portfolio/PortfolioApplication.java
@@ -0,0 +1,15 @@
+package com.portfolio.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioApplication.class, args);
+		System.out.println("Hello, Welcome to my portfolio");
+
+	}
+
+}
